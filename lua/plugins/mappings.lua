fea690eafe9791e05f9429046dfad9400a1f15f9
Alex <aconstantin86@yahoo.com>
2024-04-29 21:55:57 +0300

    random comint
    dotnet is still not working, but im getting there

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -11,8 +11,35 @@ return {
             function() require("telescope").extensions.projects.projects {} end,
             desc = "open projects",
           },
+          ["o"] = { "a<CR><ESC>", desc = "Insert new line from cursor" },
+          ["<C-F5>"] = { function() require("csharp").debug_project() end, desc = "CSharp project runner" },
+          ["-"] = { "<CMD>Oil<CR>", desc = "Open parent directory" },
+        },
+        t = {
+          ["o"] = false,
         },
       },
+      -- {
+      --   "AstroNvim/astrolsp",
+      --   ---@type AstroLSPOpts
+      --   opts = {
+      --     mappings = {
+      --       n = {
+      --         -- this mapping will only be set in buffers with an LSP attached
+      --         K = {
+      --           function() vim.lsp.buf.hover() end,
+      --           desc = "Hover symbol details",
+      --         },
+      --         -- condition for only server with declaration capabilities
+      --         gD = {
+      --           function() vim.lsp.buf.declaration() end,
+      --           desc = "Declaration of current symbol",
+      --           cond = "textDocument/declaration",
+      --         },
+      --       },
+      --     },
+      --   },
+      -- },
     },
   },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -12,6 +12,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
+        "omnisharp",
         -- add more arguments for adding more language servers
       })
     end,
@@ -36,6 +37,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "python",
+        "netcoredbg",
         -- add more arguments for adding more debuggers
       })
     end,
