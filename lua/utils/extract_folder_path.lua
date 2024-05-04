60a4eda3e3b55479841303b940513a1e56c19b03
Alex <aconstantin86@yahoo.com>
2024-05-04 20:03:49 +0300

    dotnet working

--- lua/utils/extract_folder_path.lua
+++ lua/utils/extract_folder_path.lua
@@ -1,9 +1,11 @@
-local extractFolderPath = function(filePath)
-  local lastSlashIndex = filePath:find "\\[^\\]*%.csproj$"
-  if lastSlashIndex then
-    return filePath:sub(1, lastSlashIndex - 1)
+local function extractFolderPath(filePath)
+  local lastBackslashIndex = filePath:find "\\[^\\]*%.csproj$"
+  if lastBackslashIndex then
+    local projectName = filePath:sub(lastBackslashIndex + 1, -8) -- Extract project name
+    local folderPath = filePath:sub(1, lastBackslashIndex - 1)
+    return folderPath, projectName
   else
-    return "Invalid file path"
+    return "Invalid file path", "Invalid project name"
   end
 end
 
