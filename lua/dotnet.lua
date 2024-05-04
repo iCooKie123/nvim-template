fc3c4842009867796a43e251b65ae3a994d4033d
Alex <aconstantin86@yahoo.com>
2024-05-04 20:03:54 +0300

    dotnet working

--- lua/dotnet.lua
+++ lua/dotnet.lua
@@ -13,9 +13,10 @@ local callback_build = function(path)
   print ""
   print("Cmd to execute: " .. cmd)
   local f = os.execute(cmd)
-  vim.g["dotnet_project_path"] = extractFolderPath(path)
+  vim.g["dotnet_project_path"], vim.g["dotnet_project_name"] = extractFolderPath(path)
+
   if f == 0 then
-    notify("Build: ✅" .. vim.g["dotnet_project_path"], "info", { title = "Build", timeout = 1000 })
+    notify("Build: ✅ ", "info", { title = "Build", timeout = 1000 })
     return true
   else
     notify("Build: ❌", "error", { title = "Build", timeout = 1000 })
@@ -31,7 +32,8 @@ dap.adapters.coreclr = {
   args = { "--interpreter=vscode" },
 }
 
-local config = {
+local config
+config = {
   {
     name = "Launch an executable",
     type = "coreclr",
@@ -42,7 +44,13 @@ local config = {
         pickers
           .new(opts, {
             prompt_title = "Path to executable",
-            finder = finders.new_oneshot_job({ "rg", "--files", "--no-ignore", "-g", "*.dll" }, {}),
+            finder = finders.new_oneshot_job({
+              "rg",
+              "--files",
+              "--no-ignore",
+              "--glob",
+              "*/bin/Debug/net*/" .. vim.g["dotnet_project_name"] .. ".dll",
+            }, {}),
             sorter = conf.generic_sorter(opts),
             attach_mappings = function(buffer_number)
               actions.select_default:replace(function()
@@ -59,30 +67,30 @@ local config = {
     env = {
       ASPNETCORE_ENVIRONMENT = "Development",
     },
-    cwd = vim.g["dotnet_project_path"],
-    -- prelaunchTask = function()
-    --   return coroutine.create(function(coro)
-    --     local opts = {}
-    --     pickers
-    --       .new(opts, {
-    --         prompt_title = "Project to build",
-    --         finder = finders.new_oneshot_job({ "rg", "--files", "--no-ignore", "-g", "*.csproj" }, {}),
-    --         sorter = conf.generic_sorter(opts),
-    --         attach_mappings = function(buffer_number)
-    --           actions.select_default:replace(function()
-    --             actions.close(buffer_number)
-    --             if callback_build(action_state.get_selected_entry()[1]) then
-    --               coroutine.resume(coro, action_state.get_selected_entry()[1])
-    --             else
-    --               coroutine.yield(nil)
-    --             end
-    --           end)
-    --           return true
-    --         end,
-    --       })
-    --       :find()
-    --   end)
-    -- end,
+    prelaunchTask = function()
+      return coroutine.create(function(coro)
+        local opts = {}
+        pickers
+          .new(opts, {
+            prompt_title = "Project to build",
+            finder = finders.new_oneshot_job({ "rg", "--files", "--no-ignore", "-g", "*.csproj" }, {}),
+            sorter = conf.generic_sorter(opts),
+            attach_mappings = function(buffer_number)
+              actions.select_default:replace(function()
+                actions.close(buffer_number)
+                if callback_build(vim.fn.getcwd() .. "\\" .. action_state.get_selected_entry()[1]) then
+                  coroutine.resume(coro, action_state.get_selected_entry()[1])
+                  config.cwd = vim.g["dotnet_project_path"]
+                else
+                  coroutine.yield(nil)
+                end
+              end)
+              return true
+            end,
+          })
+          :find()
+      end)
+    end,
   },
 }
 
