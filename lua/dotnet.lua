4bbafa48415bd66b653a38c0e493946f6bab2cc0
Alex <aconstantin86@yahoo.com>
2024-05-06 17:32:32 +0300

    updated some plugins
    one-time working c# config

--- lua/dotnet.lua
+++ lua/dotnet.lua
@@ -14,7 +14,6 @@ local callback_build = function(path)
   print("Cmd to execute: " .. cmd)
   local f = os.execute(cmd)
   vim.g["dotnet_project_path"], vim.g["dotnet_project_name"] = extractFolderPath(path)
-
   if f == 0 then
     notify("Build: ✅ ", "info", { title = "Build", timeout = 1000 })
     return true
@@ -31,69 +30,69 @@ dap.adapters.coreclr = {
   command = "C:\\Users\\acons\\AppData\\Local\\nvim-data\\mason\\packages\\netcoredbg\\netcoredbg\\netcoredbg.exe",
   args = { "--interpreter=vscode" },
 }
-
-local config
-config = {
-  {
-    name = "Launch an executable",
-    type = "coreclr",
-    request = "launch",
-    program = function()
-      return coroutine.create(function(coro)
-        local opts = {}
-        pickers
-          .new(opts, {
-            prompt_title = "Path to executable",
-            finder = finders.new_oneshot_job({
-              "rg",
-              "--files",
-              "--no-ignore",
-              "--glob",
-              "*/bin/Debug/net*/" .. vim.g["dotnet_project_name"] .. ".dll",
-            }, {}),
-            sorter = conf.generic_sorter(opts),
-            attach_mappings = function(buffer_number)
-              actions.select_default:replace(function()
-                actions.close(buffer_number)
-                coroutine.resume(coro, action_state.get_selected_entry()[1])
-              end)
-              return true
-            end,
-          })
-          :find()
-      end)
-    end,
-    args = { "--urls=https://localhost:7149;http://localhost:5148" }, --TODO: make this configurable, with all launch options
-    env = {
-      ASPNETCORE_ENVIRONMENT = "Development",
-    },
-    prelaunchTask = function()
-      return coroutine.create(function(coro)
-        local opts = {}
-        pickers
-          .new(opts, {
-            prompt_title = "Project to build",
-            finder = finders.new_oneshot_job({ "rg", "--files", "--no-ignore", "-g", "*.csproj" }, {}),
-            sorter = conf.generic_sorter(opts),
-            attach_mappings = function(buffer_number)
-              actions.select_default:replace(function()
-                actions.close(buffer_number)
-                if callback_build(vim.fn.getcwd() .. "\\" .. action_state.get_selected_entry()[1]) then
+local get_config = function()
+  return {
+    {
+      name = "Launch an executable",
+      type = "coreclr",
+      request = "launch",
+      program = function()
+        return coroutine.create(function(coro)
+          local opts = {}
+          pickers
+            .new(opts, {
+              prompt_title = "Path to executable",
+              finder = finders.new_oneshot_job({
+                "rg",
+                "--files",
+                "--no-ignore",
+                "--glob",
+                "*/bin/Debug/net*/" .. vim.g["dotnet_project_name"] .. ".dll",
+              }, {}),
+              sorter = conf.generic_sorter(opts),
+              attach_mappings = function(buffer_number)
+                actions.select_default:replace(function()
+                  actions.close(buffer_number)
                   coroutine.resume(coro, action_state.get_selected_entry()[1])
-                  config.cwd = vim.g["dotnet_project_path"]
-                else
-                  coroutine.yield(nil)
-                end
-              end)
-              return true
-            end,
-          })
-          :find()
-      end)
-    end,
-  },
-}
+                end)
+                return true
+              end,
+            })
+            :find()
+        end)
+      end,
+      args = { "--urls=https://localhost:7149;http://localhost:5148" }, --TODO: make this configurable, with all launch options
+      env = {
+        ASPNETCORE_ENVIRONMENT = "Development",
+      },
+      cwd = vim.g["dotnet_project_path"],
+      prelaunchTask = function()
+        return coroutine.create(function(coro)
+          local opts = {}
+          pickers
+            .new(opts, {
+              prompt_title = "Project to build",
+              finder = finders.new_oneshot_job({ "rg", "--files", "--no-ignore", "-g", "*.csproj" }, {}),
+              sorter = conf.generic_sorter(opts),
+              attach_mappings = function(buffer_number)
+                actions.select_default:replace(function()
+                  actions.close(buffer_number)
+                  if callback_build(vim.fn.getcwd() .. "\\" .. action_state.get_selected_entry()[1]) then
+                    coroutine.resume(coro, action_state.get_selected_entry()[1])
+                  else
+                    coroutine.yield(nil)
+                  end
+                end)
+                return true
+              end,
+            })
+            :find()
+        end)
+      end,
+    },
+  }
+end
 
-dap.configurations.cs = config
-dap.configurations.fsharp = config
-dap.configurations.netcoredbg = config
+dap.configurations.cs = get_config()
+dap.configurations.fsharp = get_config()
+dap.configurations.netcoredbg = get_config()
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason plugins
 
 ---@type LazySpec
@@ -11,7 +9,6 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "lua_ls",
         "omnisharp",
         -- add more arguments for adding more language servers
       })
@@ -24,8 +21,10 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "prettier",
+        "prettierd",
         "stylua",
+        "csharpier",
+
         -- add more arguments for adding more null-ls sources
       })
     end,
